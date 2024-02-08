52e07cef3334db1b4204db75e037ec5c175b836d
Victor <victorac2324@gmail.com>
2024-02-07 22:56:10 -0500

    used dom attributes to add/updated index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+main.remove();
+
+const newHeader = document.createElement("h1");
+
+
+newHeader.setAttribute('id', "victory")
+
+
+newHeader.textContent ="Victor is the champion";
+
+
